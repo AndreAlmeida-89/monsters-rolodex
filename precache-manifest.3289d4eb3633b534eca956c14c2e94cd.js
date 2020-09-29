4b6337c22ed8492cef25377f515c33f665ecb835
André Felipe de Sousa Almeida <andre_rdo@hotmail.com>
2020-09-29 13:24:21 -0300

    Updates

---
+++ precache-manifest.3289d4eb3633b534eca956c14c2e94cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10eaf62a96ef5585615dfe5fa33944c2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a97702319e7240f4c37d",
+    "url": "/monsters-rolodex/static/css/main.c5675c69.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97702319e7240f4c37d",
+    "url": "/monsters-rolodex/static/js/main.11814ae0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
